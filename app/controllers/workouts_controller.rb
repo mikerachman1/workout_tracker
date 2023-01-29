04e805de80d31ffd36636bb830650d3d023a804d
Mike Rachman <mikerachman1@gmail.com>
2023-01-29 13:50:31 -0500

    show route added for workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,6 +2,10 @@ class WorkoutsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @workouts = Workout.all
+    @workouts = current_user.workouts
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
   end
 end
